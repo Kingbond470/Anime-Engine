6c422498520f45a70f229ef3ba940cb7d9657dc1
Kingbond470 <mausamsingh470@gmail.com>
2021-09-12 21:18:55 +0530

    Quiz Question Activity | Selected Option and Default Option

--- KidsQuiz/app/src/main/java/com/example/kidsquiz/QuizQuestionActivity.kt
+++ KidsQuiz/app/src/main/java/com/example/kidsquiz/QuizQuestionActivity.kt
@@ -1,16 +1,109 @@
 package com.example.kidsquiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedPosition: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
-        // object contant is available everywhere
-        val questionList=Constant.getQuestions()
-        Log.i("Question size","${questionList.size}")
+        // object constant is available everywhere
+        mQuestionList = Constant.getQuestions()
+//        Log.i("Question size","${questionList.size}")
+
+        setQuesiton()
+
+        tvOptionOne.setOnClickListener(this)
+        tvOptionTwo.setOnClickListener(this)
+        tvOptionThree.setOnClickListener(this)
+        tvOptionFour.setOnClickListener(this)
+
     }
+
+
+    private fun setQuesiton() {
+
+        mCurrentPosition = 1
+        val question: Question? = mQuestionList!![mCurrentPosition - 1]
+
+        defaultOptionView()
+
+        progressBar.progress = mCurrentPosition
+        tvProgress.text = "$mCurrentPosition" + "/" + progressBar.max
+
+        tvQuestionId.text = question!!.question
+        ivImage.setImageResource(question.image)
+        tvOptionOne.text = question.optionOne
+        tvOptionTwo.text = question.optionTwo
+        tvOptionThree.text = question.optionThree
+        tvOptionFour.text = question.optionFour
+    }
+
+    private fun defaultOptionView() {
+        val options = ArrayList<TextView>()
+        options.add(0, tvOptionOne)
+        options.add(1, tvOptionTwo)
+        options.add(2, tvOptionThree)
+        options.add(3, tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7a8089"))
+            // type face , in which text is displayed
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tvOptionOne -> {
+                selectedOptionView(tvOptionOne, 1)
+            }
+            R.id.tvOptionTwo -> {
+                selectedOptionView(tvOptionTwo, 2)
+            }
+            R.id.tvOptionThree -> {
+                selectedOptionView(tvOptionThree, 3)
+            }
+            R.id.tvOptionFour -> {
+                selectedOptionView(tvOptionFour, 4)
+            }
+
+
+        }
+
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionView()
+        mSelectedPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363a43"))
+        // type face , in which text is displayed to be bold
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
 }
